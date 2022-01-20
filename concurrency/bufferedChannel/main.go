6ccc52ba4673e2d10fbdfbd2153b05aafe29cb87
Jim <cushiony26@gmail.com>
2022-01-20 22:45:47 +0800

    gogogo

--- concurrency/bufferedChannel/main.go
+++ concurrency/bufferedChannel/main.go
@@ -2,19 +2,26 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	c := make(chan int)
+	c := make(chan int, 2)
 	go func() {
-		time.Sleep(2 * time.Second)
-		fmt.Println("receive channel data")
-		<-c
+		//time.Sleep(2 * time.Second)
+		fmt.Println("receive channel data ", <-c)
+		fmt.Println("receive channel data ", <-c)
+		// fmt.Println("receive channel data ", <-c)
 	}()
 	data := 666
 	fmt.Println("send channel data -> ", data)
 	c <- data
-	c <- data
+	c <- data + 1
+	c <- data + 2
+	c <- data + 3
+	c <- data + 4
 	fmt.Println("send finished")
+	// time.Sleep(10 * time.Second)
+	// fmt.Println("receive channel data ", <-c)
+	// fmt.Println("receive channel data ", <-c)
+	// fmt.Println("receive channel data ", <-c)
 }
